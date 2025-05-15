7d65f39bd4f7e2aba25bc9f2cafa6a8ac0f4371d
Taylo <Taylo@MARIEL>
2025-05-15 16:41:19 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// Develop program using Queue.
+// create queue of callers
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Queue<string> callers = new Queue<string>();
+        callers.Enqueue("Taylor");
+        callers.Enqueue("Alexander");
+        callers.Enqueue(" Johnathan");
+        callers.Enqueue("Tiffany");
+        foreach (var item in callers)
+        {
+            Console.WriteLine(item + " ");
+        }
+        Console.WriteLine();
+            callers.Dequeue();
+            Console.WriteLine("after dq:  ");
+            foreach( var item in callers)
+            {
+                Console.WriteLine(item + " ");
+            }
+            Console.ReadKey();
+        }
+    }
